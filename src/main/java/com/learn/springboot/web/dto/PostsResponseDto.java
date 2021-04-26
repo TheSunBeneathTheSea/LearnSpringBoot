820abc59a94c403082697c8c39c19f924f09f2a2
DESKTOP-GDMTS6V\USER <slowlight50@gmail.com>
2021-04-26 20:27:26 +0900

    update

---
+++ src/main/java/com/learn/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.learn.springboot.web.dto;
+
+import com.learn.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+    private String IPAddress;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+        this.IPAddress = entity.getIPAddress();
+    }
+}
